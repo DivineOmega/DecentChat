7dcd4587b8f7c6adbfa525d31dbb84caa87a6755
Jordan Hall <jordan.hall@rapidweb.biz>
2016-01-29 14:32:00 +0000

    My DM address retrieval and display

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+function populateMyDmAddress()
+{
+  var net = require('net');
+
+  var client = net.connect({ port: 8881 }, () => {
+    console.log('Connected (to get my DM address)');
+
+  });
+  client.on('data', (data) => {
+    data = data.toString();
+
+    if (data.startsWith('*')) {
+      if (data.startsWith('*100')) {
+        client.write('me\n');
+      } else if (data.startsWith('*330')) {
+        var dataParts = data.split('\n');
+        localStorage.dmAddress = dataParts[1];
+        $('#dmAddress').val(localStorage.dmAddress);
+        console.log("Got my DM address");
+        client.end();
+      }
+      return;
+    }
+
+  });
+  client.on('end', () => {
+    console.log('Disconnected');
+  });
+}
+
+$('#dmAddress').on('click', function() {
+  $(this).select();
+});
+
+$(document).ready(function() {
+    populateMyDmAddress();
+});
