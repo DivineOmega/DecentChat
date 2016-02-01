8f898d0daabb5f95496199de8fac4ad28a3d9dca
Jordan Hall <jordan.hall@rapidweb.biz>
2016-02-01 14:24:53 +0000

    Contact addition, deletion and listing

--- js/main.js
+++ js/main.js
@@ -29,10 +29,98 @@ function populateMyDmAddress()
   });
 }
 
-$('#dmAddress').on('click', function() {
+function getContacts()
+{
+  var contacts = null;
+
+  try {
+    contacts = JSON.parse(localStorage.contacts);
+  } catch (e) {
+    contacts = [];
+  }
+
+  return contacts;
+}
+
+function saveContacts(contacts)
+{
+  localStorage.contacts = JSON.stringify(contacts);
+}
+
+function addContact(name, dmAddress)
+{
+  var contacts = getContacts();
+
+  var contact = { "name": name, "dmAddress": dmAddress };
+
+  contacts.push(contact);
+
+  saveContacts(contacts);
+  updateContactsUI();
+}
+
+function deleteContact(index)
+{
+  var contacts = getContacts();
+
+  contacts.splice(index, 1);
+
+  saveContacts(contacts);
+  updateContactsUI();
+}
+
+function updateContactsUI()
+{
+  var contacts = getContacts();
+
+  $('#contactsList').html($('#contactsListStart').html());
+
+  for (var i = 0; i < contacts.length; i++) {
+    var contact = contacts[i];
+
+    var html = $('#contactsListTemplate').html();
+
+    html = html.replaceAll("[[name]]", contact.name);
+    html = html.replaceAll("[[index]]", i);
+
+    $('#contactsList').append(html);
+  }
+}
+
+$(document).on('click', '#dmAddress', function() {
   $(this).select();
+});
+
+$(document).on('click', '#addContactButton', function() {
+  var name = $('#nameToAdd').val();
+  var dmAddress = $('#dmAddressToAdd').val();
+
+  if (name && dmAddress) {
+    addContact(name, dmAddress);
+  }
+
+  $('#nameToAdd').val('');
+  $('#dmAddressToAdd').val('');
+});
+
+$(document).on('click', '.contactDeleteButton', function() {
+  var id = $(this).attr('id');
+  var idParts = id.split('_');
+  var index = idParts[1];
+
+  var contacts = getContacts();
+
+  if (confirm('Delete your contact \''+contacts[index].name+'\'?')) {
+    deleteContact(index);
+  }
 });
 
 $(document).ready(function() {
     populateMyDmAddress();
+    updateContactsUI();
 });
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
