c1bbc37052f1f19a4b6ad5b6ed44bf2c958ece6b
Jordan Hall <jordan.hall@rapidweb.biz>
2016-02-11 14:20:34 +0000

    Build script for Linux

---
+++ builds/linux_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Clean up existing build if present
+rm -r DecentChat-linux-x64
+
+# Download electron (Linux x64)
+wget -nc https://github.com/atom/electron/releases/download/v0.36.7/electron-v0.36.7-linux-x64.zip
+unzip electron-v0.36.7-linux-x64.zip -d DecentChat-linux-x64
+
+# Rename electron executable
+mv DecentChat-linux-x64/electron DecentChat-linux-x64/DecentChat
+
+# Make app directory
+mkdir DecentChat-linux-x64/resources/app
+
+# Sync *.html and *.js
+rsync ../* DecentChat-linux-x64/resources/app/ -r --exclude builds
+
+# Install dependencies
+cd DecentChat-linux-x64/resources/app/
+bower install bootstrap
+cd ../../../
