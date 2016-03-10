857aaa6e41054df31522f7cd5da74980bfc12e1b
Jordan Hall <jordan.hall@rapidweb.biz>
2016-03-10 11:34:11 +0000

    Windows build script

---
+++ builds/windows_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Clean up existing build if present
+rm -r DecentChat-windows-x64
+
+# Download electron (Windows x64)
+wget -nc https://github.com/atom/electron/releases/download/v0.36.7/electron-v0.36.7-win32-x64.zip
+unzip electron-v0.36.7-win32-x64.zip -d DecentChat-windows-x64
+
+# Rename electron executable
+mv DecentChat-windows-x64/electron.exe DecentChat-windows-x64/DecentChat.exe
+
+# Make app directory
+mkdir DecentChat-windows-x64/resources/app
+
+# Sync *.html and *.js
+rsync ../* DecentChat-windows-x64/resources/app/ -r --exclude builds
+
+# Install dependencies
+cd DecentChat-windows-x64/resources/app/
+bower install bootstrap
+cd ../../../
